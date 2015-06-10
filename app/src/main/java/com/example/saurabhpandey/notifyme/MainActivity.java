9a4d1816e30b268ed5f7974a6fa70bd92b701f58
SaurabhPandey <saurabh.joy8@gmail.com>
2015-06-10 02:04:45 -0700

    New Splash

--- app/src/main/java/com/example/saurabhpandey/notifyme/MainActivity.java
+++ app/src/main/java/com/example/saurabhpandey/notifyme/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.saurabhpandey.notifyme;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -14,7 +16,18 @@ import android.view.View;
 public class MainActivity extends Activity {
     private static final int uniqueID = 123654;
     NotificationCompat.Builder notification;
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Really Exit?")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).create().show();}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
